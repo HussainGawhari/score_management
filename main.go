44a311d43d21cad01736628b37a20ff9d2cdeb60
HussainGawhari <74446438+HussainGawhari@users.noreply.github.com>
2023-08-06 20:48:24 +0530

    Update main.go

--- main.go
+++ main.go
@@ -2,30 +2,31 @@ package main
 
 import (
 	"fmt"
-	"spotbuzz-backend/pkg/dbhelper"
-	"spotbuzz-backend/router"
+	// "spotbuzz-backend/pkg/dbhelper"
+	// "spotbuzz-backend/router"
 
-	"github.com/gin-gonic/gin"
-	_ "github.com/lib/pq"
+	// "github.com/gin-gonic/gin"
+	// _ "github.com/lib/pq"
 )
 
 func main() {
 
+	fmt.Println("This is mY player score management systemK")
 	// DB connection
-	db, err := dbhelper.CreateConnection()
-	if err != nil {
-		return
-	}
-	//    incase if you need to run locally uncommented this line
-	tab, err := dbhelper.CreateTable("Player")
-	fmt.Println(tab)
-	// database connection close
-	defer db.Close()
-	// create instance of gin framwork
-	r := gin.New()
-	// register routes to handle router
-	router.Router(r)
-	// server running on port 8000
-	r.Run(":8080")
+	// db, err := dbhelper.CreateConnection()
+	// if err != nil {
+	// 	return
+	// }
+	// //    incase if you need to run locally uncommented this line
+	// tab, err := dbhelper.CreateTable("Player")
+	// fmt.Println(tab)
+	// // database connection close
+	// defer db.Close()
+	// // create instance of gin framwork
+	// r := gin.New()
+	// // register routes to handle router
+	// router.Router(r)
+	// // server running on port 8000
+	// r.Run(":8080")
 
 }
